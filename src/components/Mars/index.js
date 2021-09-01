61e8d6515246caa2eca33a2c6f1af789433b87d0
Dimitri-codePop <dimmartin32@gmail.com>
2021-09-01 20:22:12 +0200

    Projet fini plus que le css a faire

--- src/components/Mars/index.js
+++ src/components/Mars/index.js
@@ -1,31 +1,43 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './styles.css';
 
-import PropTypes from 'prop-types';
-
 export default function Mars() {
-  const [ fetchImage, setFetchImage ] = useState([]);
-  const [ date, setDate] = useState("");
+  const [fetchImage, setFetchImage] = useState([]);
+  const [image, setImage] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(async () => {
-    const now = new Date().toISOString().split('T')[0];
-    const result = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2020-08-01&api_key=TGw3yb3SYCjTaMtCghavGF0t5Nu0rCZh963TyHzw`);
-    setDate(now);
-    setFetchImage(result.data.photos);
-  }, [])
+    try {
+      const yesterday = new Date((new Date()).valueOf() - 1000*60*60*24).toISOString().split('T')[0];
 
-  console.log(fetchImage);
-  return(
+      const result = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${yesterday}&api_key=TGw3yb3SYCjTaMtCghavGF0t5Nu0rCZh963TyHzw`);
+      if (result.data.photos.length < 1) {
+        setMessage('Aucune photo prise hier par notre copain le Rover. ');
+      }
+      setFetchImage(result.data.photos);
+    }
+    catch (error) {
+      console.log(error);
+      setMessage('Une erreur serveur est survenue');
+    }
+  }, []);
+
+  useEffect(() => {
+    const images = fetchImage.map((objet, i) => {
+      return(
+        <div key={i}>
+          <img src={objet.img_src} alt={objet.id} />
+        </div>
+      )
+    })
+    setImage(images);
+  }, [fetchImage]);
+  return (
     <>
-
+      {image}
+      <p>{message}</p>
     </>
   );
-  
-  
 }
-
-Mars.propTypes = {
-
-};
